07b6cc5f793a566f5e63702101c6b92757b91edb
gson Liang <yuanyu90221@gmail.com>
2023-06-18 23:16:53 +0800

    feat(init long block example): init long block example
    init long block example

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,24 +3,25 @@ import { Worker } from 'worker_threads';
 import * as path from 'path';
 import { THREAD_COUNT } from './constant';
 import { FourWorkerCreateService } from './four-worker.service';
+import { heavilyJob } from './heavily-job';
 @Injectable()
 export class AppService {
   constructor(private readonly fourWorkerService: FourWorkerCreateService) {}
   async blocking() {
-    const workerPromises: Promise<number>[] = [];
-    for (let i = 0; i < THREAD_COUNT; i++) {
-      workerPromises.push(this.fourWorkerService.createWorker());
-    }
-    const thread_results = await Promise.all(workerPromises);
-    const total =
-      thread_results[0] +
-      thread_results[1] +
-      thread_results[2] +
-      thread_results[3];
-    return {
-      data: total,
-      message: 'this is block service',
-    };
+    // const workerPromises: Promise<number>[] = [];
+    // for (let i = 0; i < THREAD_COUNT; i++) {
+    //   workerPromises.push(this.fourWorkerService.createWorker());
+    // }
+    // const thread_results = await Promise.all(workerPromises);
+    // const total =
+    //   thread_results[0] +
+    //   thread_results[1] +
+    //   thread_results[2] +
+    //   thread_results[3];
+    // return {
+    //   data: total,
+    //   message: 'this is block service',
+    // };
     // const workerPromise: Promise<number> = new Promise<number>(
     //   (resolve, reject) => {
     //     const worker = new Worker(path.join(__dirname, './worker.js'));
@@ -33,10 +34,11 @@ export class AppService {
     //   },
     // );
     // const count = await workerPromise;
-    // return {
-    //   count,
-    //   message: 'this is block service',
-    // };
+    const count = heavilyJob();
+    return {
+      count,
+      message: 'this is block service',
+    };
   }
   nonBlocking(): string {
     return 'This is non-block service';
