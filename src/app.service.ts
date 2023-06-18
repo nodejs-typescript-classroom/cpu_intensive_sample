eb188c4fd173d8b3a82d6fc5c97e624eea697b03
gson Liang <yuanyu90221@gmail.com>
2023-06-18 23:33:21 +0800

    feat(four-worker-thread): four-worker-thread
    four-worker-thread

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,37 +8,37 @@ import { heavilyJob } from './heavily-job';
 export class AppService {
   constructor(private readonly fourWorkerService: FourWorkerCreateService) {}
   async blocking() {
-    // const workerPromises: Promise<number>[] = [];
-    // for (let i = 0; i < THREAD_COUNT; i++) {
-    //   workerPromises.push(this.fourWorkerService.createWorker());
-    // }
-    // const thread_results = await Promise.all(workerPromises);
-    // const total =
-    //   thread_results[0] +
-    //   thread_results[1] +
-    //   thread_results[2] +
-    //   thread_results[3];
-    // return {
-    //   data: total,
-    //   message: 'this is block service',
-    // };
-    const workerPromise: Promise<number> = new Promise<number>(
-      (resolve, reject) => {
-        const worker = new Worker(path.join(__dirname, './worker.js'));
-        worker.on('message', (data: number) => {
-          resolve(data);
-        });
-        worker.on('error', (error) => {
-          reject(error);
-        });
-      },
-    );
-    const count = await workerPromise;
-    // const count = heavilyJob();
+    const workerPromises: Promise<number>[] = [];
+    for (let i = 0; i < THREAD_COUNT; i++) {
+      workerPromises.push(this.fourWorkerService.createWorker());
+    }
+    const thread_results = await Promise.all(workerPromises);
+    const total =
+      thread_results[0] +
+      thread_results[1] +
+      thread_results[2] +
+      thread_results[3];
     return {
-      count,
+      data: total,
       message: 'this is block service',
     };
+    // const workerPromise: Promise<number> = new Promise<number>(
+    //   (resolve, reject) => {
+    //     const worker = new Worker(path.join(__dirname, './worker.js'));
+    //     worker.on('message', (data: number) => {
+    //       resolve(data);
+    //     });
+    //     worker.on('error', (error) => {
+    //       reject(error);
+    //     });
+    //   },
+    // );
+    // const count = await workerPromise;
+    // const count = heavilyJob();
+    // return {
+    //   count,
+    //   message: 'this is block service',
+    // };
   }
   nonBlocking(): string {
     return 'This is non-block service';
